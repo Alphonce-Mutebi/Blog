be695843563c9918aed08db85fb7b9866cdf4889
Alphonce Mutebi <mutebialphonce@gmail.com>
2019-12-11 14:21:21 +0300

    admin model

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, default='Blank')
     content = models.TextField()
     date_posted = models.DateTimeField(default = timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
